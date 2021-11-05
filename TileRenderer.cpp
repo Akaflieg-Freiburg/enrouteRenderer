ac9bac8a4772751437da2ae68c308f1dce176127
Stefan Kebekus <stefan.kebekus@math.uni-freiburg.de>
2021-11-05 15:36:15 +0100

    Begin with vector tiles: decompression

--- TileRenderer.cpp
+++ TileRenderer.cpp
@@ -1,7 +1,9 @@
 
+#include "compression.h"
 #include "TileRenderer.h"
 
 #include <QDebug>
+#include <QFile>
 #include <QImage>
 #include <QSqlDatabase>
 #include <QSqlQuery>
@@ -18,7 +20,8 @@ TileRenderer::TileRenderer(QObject* parent)
 QImage TileRenderer::render(int zoom, int column, int row)
 {
     //QString fileName("/home/kebekus/Austausch/ICAO+VFR+DE+21Q1.mbtiles");
-    QString fileName("/home/kebekus/Austausch/ed_256.mbtiles");
+    //QString fileName("/home/kebekus/Austausch/ed_256.mbtiles");
+    QString fileName("/home/kebekus/Austausch/aviation_maps/Europe/Germany.mbtiles");
 
     auto db = QSqlDatabase::database(fileName);
     if (!db.isValid()) {
@@ -33,14 +36,29 @@ QImage TileRenderer::render(int zoom, int column, int row)
 
     auto rowFlipped = (1 << zoom)-1-row;
     QString queryString = QString("select zoom_level, tile_column, tile_row, tile_data from tiles where zoom_level=%1 and tile_column=%2 and tile_row=%3;").arg(zoom).arg(column).arg(rowFlipped);
-
     QSqlQuery query(db);
     query.exec(queryString);
-
     if (!query.first()) {
         return {};
     }
-
     QByteArray tileData = query.value(3).toByteArray();
-    return QImage::fromData(tileData);
+    if (tileData.isEmpty()) {
+        return {};
+    }
+
+    // Check if data is image. In that case, our job is easy.
+    auto image = QImage::fromData(tileData, "PNG");
+    if (!image.isNull()) {
+        return image;
+    }
+
+    auto uncompressedData = compression::gzUncompress(tileData);
+    qWarning() << tileData.size() << uncompressedData.size();
+
+    QFile file("/home/kebekus/experiment/t.data");
+    file.open(QIODeviceBase::WriteOnly);
+    file.write(tileData);
+    file.close();
+
+    return {};
 }
